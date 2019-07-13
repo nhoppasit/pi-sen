dff9ea51e747326d0e0202bac1a76c0efa898108
Nhoppasit <nhoppasit@gmail.com>
2019-07-13 22:00:33 +0700

    Download c# from nhoppasit/co2-pi

---
+++ data/CO2Sensor/CO2 Sensor/CO2 Sensor/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace CO2_Sensor
+{
+    partial class co2_sensor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label3 = new System.Windows.Forms.Label();
+            this.baud_list = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lisport = new System.Windows.Forms.ComboBox();
+            this.discon = new System.Windows.Forms.Button();
+            this.connect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(214, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 20);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "Disconnect";
+            // 
+            // baud_list
+            // 
+            this.baud_list.BackColor = System.Drawing.Color.Gray;
+            this.baud_list.Location = new System.Drawing.Point(74, 41);
+            this.baud_list.Name = "baud_list";
+            this.baud_list.Size = new System.Drawing.Size(114, 21);
+            this.baud_list.TabIndex = 34;
+            // 
+            // label2
+            // 
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(10, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 23);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Baud";
+            // 
+            // lisport
+            // 
+            this.lisport.BackColor = System.Drawing.Color.Gray;
+            this.lisport.Location = new System.Drawing.Point(74, 12);
+            this.lisport.Name = "lisport";
+            this.lisport.Size = new System.Drawing.Size(114, 21);
+            this.lisport.TabIndex = 33;
+            // 
+            // discon
+            // 
+            this.discon.BackColor = System.Drawing.Color.Gray;
+            this.discon.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.discon.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.discon.Location = new System.Drawing.Point(218, 33);
+            this.discon.Name = "discon";
+            this.discon.Size = new System.Drawing.Size(88, 28);
+            this.discon.TabIndex = 32;
+            this.discon.Text = "Disconnect";
+            this.discon.UseVisualStyleBackColor = false;
+            this.discon.Click += new System.EventHandler(this.discon_Click);
+            // 
+            // connect
+            // 
+            this.connect.BackColor = System.Drawing.Color.Gray;
+            this.connect.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
+            this.connect.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.connect.Location = new System.Drawing.Point(218, 4);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(88, 28);
+            this.connect.TabIndex = 31;
+            this.connect.Text = "Connect";
+            this.connect.UseVisualStyleBackColor = false;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(10, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 23);
+            this.label1.TabIndex = 36;
+            this.label1.Text = "ComPort";
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox2.Location = new System.Drawing.Point(411, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 35);
+            this.textBox2.TabIndex = 39;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(798, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 36);
+            this.button1.TabIndex = 41;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(15, 99);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 5;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.IsXValueIndexed = true;
+            series1.Legend = "Legend1";
+            series1.Name = "PPM";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(746, 370);
+            this.chart1.TabIndex = 42;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(798, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 38);
+            this.button2.TabIndex = 43;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label5.Location = new System.Drawing.Point(517, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 25);
+            this.label5.TabIndex = 44;
+            this.label5.Text = "PPM";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label6.Location = new System.Drawing.Point(347, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 25);
+            this.label6.TabIndex = 45;
+            this.label6.Text = "CO2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox1.Location = new System.Drawing.Point(411, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 35);
+            this.textBox1.TabIndex = 46;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label4.Location = new System.Drawing.Point(335, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 25);
+            this.label4.TabIndex = 47;
+            this.label4.Text = "Temp";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label7.Location = new System.Drawing.Point(517, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(28, 25);
+            this.label7.TabIndex = 48;
+            this.label7.Text = "C";
+            // 
+            // co2_sensor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(935, 481);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.baud_list);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lisport);
+            this.Controls.Add(this.discon);
+            this.Controls.Add(this.connect);
+            this.Controls.Add(this.label1);
+            this.Name = "co2_sensor";
+            this.Text = "CO2 Sensor";
+            this.Load += new System.EventHandler(this.co2_sensor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox baud_list;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox lisport;
+        private System.Windows.Forms.Button discon;
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ data/CO2Sensor/CO2 Sensor/CO2 Sensor/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+namespace CO2_Sensor
+{
+    public partial class co2_sensor : Form
+    {
+        public delegate void InvokeDelegate();
+        int co2_state;
+        const byte STARTBIT = 0xFF;
+        const byte DATABIT  = 2;
+        const byte CHECK_VAL = 3;
+        int  Highchannel=2, Lowchannel=3;
+        byte[] databuf = new byte[20];
+        int cnt_data_in = 0;
+        int xx=0,xn=0;
+        public co2_sensor()
+        {
+            InitializeComponent();
+        }
+
+        private void connect_Click(object sender, EventArgs e)
+        {
+            if (lisport.Items.Count > 0) // If there are ports available
+            {
+                serialPort.BaudRate = int.Parse(baud_list.Text);
+                serialPort.PortName = lisport.Text;
+                if (!serialPort.IsOpen)
+                {
+                    serialPort.Open();
+                    label3.Text = "Connect";
+                }
+            }
+
+        }
+
+        private void discon_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close(); // Close port
+                label3.Text = "Disconnect";
+            }
+        }
+
+        private void serialPort_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            textBox2.BeginInvoke(new InvokeDelegate(updateTextbox));
+        }
+        private void updateTextbox()
+        {
+            int inputByte = serialPort.BytesToRead;
+            byte[] bytes = new byte[inputByte];
+            serialPort.Read(bytes, 0, inputByte);
+            for (int i = 0; i < inputByte; i++)
+            {
+                databuf[cnt_data_in] = bytes[i];
+                cnt_data_in++;
+            }
+            if (cnt_data_in > 8)
+            {
+                process_data();
+                cnt_data_in = 0;
+            }
+        }
+        private void process_data()
+        {
+            if (chk_sum() == databuf[8])
+            { 
+                //Gas Concentration = High channel*256+low channel, No.of sensor: 0x01
+                int ppm = databuf[Highchannel] * 256 + databuf[Lowchannel];
+                int temp = databuf[4];
+                temp = temp - 40;
+                textBox1.Text = temp.ToString();
+                textBox2.Text = ppm.ToString();
+                update_grapg(ppm);
+            }
+        }
+        private byte chk_sum()
+        {
+            byte sum = 0;
+            for (int i = 1; i < 8; i++)
+            {
+                sum += databuf[i];
+            }
+            byte cal_sum = (byte)(sum & 0xFF);
+            cal_sum = (byte)((~cal_sum) + 1);
+            return (cal_sum);
+        }
+        void update_grapg(int gas)
+        {
+            chart1.Series["PPM"].Points.AddY(gas);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            xx = 0;
+            timer1.Enabled=true;
+        }
+
+        private void co2_sensor_Load(object sender, EventArgs e)
+        {
+            string[] s = SerialPort.GetPortNames();
+            int i = 0;
+
+            baud_list.Items.Clear();
+            baud_list.Items.Add("1200");
+            baud_list.Items.Add("2400");
+            baud_list.Items.Add("4800");
+            baud_list.Items.Add("9600");
+            baud_list.Items.Add("19200");
+            baud_list.Items.Add("38400");
+            baud_list.Items.Add("57600");
+            baud_list.Items.Add("115200");
+            baud_list.Items.Add("230400");
+            baud_list.SelectedIndex = 3;
+            if (s.Length > 0)
+            {
+                lisport.Items.Clear();
+                foreach (string port in s)
+                {
+                    lisport.Items.Add(s[i]);
+                    i++;
+                }
+                lisport.SelectedIndex = 0;
+
+            }
+            else
+            {
+                MessageBox.Show("Serial port not found");
+                button1.Enabled = false;
+                button2.Enabled = false;
+                connect.Enabled = false;
+                discon.Enabled = false;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            byte[] data = { 0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79 };
+            serialPort.Write(data, 0, 9);
+        
+        }
+
+        
+    }
+}
