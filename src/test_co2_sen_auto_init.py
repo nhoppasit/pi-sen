e75ad40fd63ca3bccd712e83c936b5f9512f569b
Nhoppasit <nhoppasit@gmail.com>
2019-07-14 15:00:57 +0700

    Test chk sum. Found not work for cast byte in python.

--- src/test_co2_sen_auto_init.py
+++ src/test_co2_sen_auto_init.py
@@ -4,8 +4,16 @@ Created on Jul 14, 2019
 @author: think
 '''
 
-def co2_chk_sum():
+def co2_chk_sum(databuf):
     print "CO2 sensor check sum."
+    sum = 0;
+    for i in range(1,8):
+        print repr(i);
+        sum = sum + databuf[i];
+        print "Sum = " + str(sum);
+    cal_sum = (sum & 0xFF);
+    cal_sum = ((~cal_sum) + 1);
+    print "Sum = " + str(cal_sum);
     
 def main():
 
@@ -13,6 +21,8 @@ def main():
     print "Test CO2 sensor auto initialization."
     print "======================================================================="
 
+    co2_chk_sum([0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79]);
+    
 if __name__ == '__main__':
     main();
     pass
