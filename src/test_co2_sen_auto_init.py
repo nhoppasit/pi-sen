e6156fbc3abac2051b5983fcecb0d57020f185f8
nhoppasit <s.saejia@gmail.com>
2019-07-14 23:34:39 +0700

    Formatting code and test. It's OK.

--- src/test_co2_sen_auto_init.py
+++ src/test_co2_sen_auto_init.py
@@ -13,11 +13,12 @@ def co2_chk_sum(databuf):
     """
     Data buffer is string from serial port.
     """
+    print "Checksum calculating..."
     sum = int(0)
     for i in range(1,8):
-        print repr(i)
+        # print repr(i)
         sum = sum + ord(databuf[i])
-        print "Sum = " + str(sum)
+        # print "Sum = " + str(sum)
     cal_sum = (sum & 0xFF)
     cal_sum = ((~cal_sum) + 1) & 0xFF
     print "Calculated Sum = " + str(cal_sum)
@@ -40,28 +41,30 @@ def main():
             ser = serial.Serial(port,9600,timeout=0)
             print ser
             sleep(0.1)
-            if ser.is_open:
-                ser.write([0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79])
-                sleep(0.1)
-                databuf = ser.read(20)
-                ser.close()
-                print "databuff => " + repr(databuf)
-                print "length = " + str(len(databuf))
-                
-                # Validation
-                if len(databuf)!=9:
-                    print "Wrong port response!"
-                    continue     
-                else:
-                    print "Good response."           
-                if co2_chk_sum(databuf) != ord(databuf[8]):
-                    print "Wrong checksum of " + str(ord(databuf[8])) + "!"
-                    continue
-                else:
-                    print "Checksum OK."
+            if not ser.is_open: continue
+            ser.write([0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79])
+            sleep(0.1)
+            databuf = ser.read(20)
+            ser.close()
+            print "databuff => " + repr(databuf)
+            print "length = " + str(len(databuf))
+            
+            # Validation
+            if len(databuf)!=9:
+                print "Wrong port response!"
+                continue     
+            else:
+                print "Good response."           
+            if co2_chk_sum(databuf) != ord(databuf[8]):
+                print "Wrong checksum of " + str(ord(databuf[8])) + "!"
+                continue
+            else:
+                print "Checksum OK."
 
-                # Keep
-                port_co2 = port                    
+            # Keep
+            port_co2 = port
+
+                                
         except Exception as ex:
             logtext = "ERROR: initial serial port! " + repr(ex) 
             print logtext
