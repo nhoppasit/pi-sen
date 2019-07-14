1db068e275163734a581a04c392ed048f9dd7e0d
Nhoppasit <nhoppasit@gmail.com>
2019-07-14 13:34:27 +0700

    Initial co2 command

---
+++ src/test_co2_sen_auto_init.py
@@ -0,0 +1,18 @@
+'''
+Created on Jul 14, 2019
+
+@author: think
+'''
+
+def co2_chk_sum():
+    print "CO2 sensor check sum."
+    
+def main():
+
+    print "\n======================================================================="
+    print "Test CO2 sensor auto initialization."
+    print "======================================================================="
+
+if __name__ == '__main__':
+    main();
+    pass
